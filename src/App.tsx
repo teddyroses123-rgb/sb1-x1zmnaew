c606b8ae08dbf9e8fe78f752c5db78bee232a954
teddyroses123-rgb <teddyroses123@gmail.com>
2025-09-11 17:58:38 +0300

    Полностью удалить хедер

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Hero from './components/Hero';
 import WhatInside from './components/WhatInside';
 import ForWhom from './components/ForWhom';
 import SpecialOffer from './components/SpecialOffer';
@@ -11,7 +10,6 @@ import FinalBlock from './components/FinalBlock';
 function App() {
   return (
     <div className="min-h-screen">
-      <Hero />
       <WhatInside />
       <ForWhom />
       <SpecialOffer />
