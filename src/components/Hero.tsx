db79eba5f9272e2199ae4c57f344bd920213172f
teddyroses123-rgb <teddyroses123@gmail.com>
2025-09-11 16:30:53 +0300

    Восстановить правильный контент Hero компонента

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,7 +42,7 @@ const Hero = () => {
       {/* Background with blur */}
       <div className="absolute inset-0">
         <img 
-          src="https://i.ibb.co/PvN7wXY7/image.png" 
+          src="https://i.ibb.co/V0gMw7fn/E6-DCA96-B-AF56-4-CA1-BAB1-3-C95196837-D0-no-bg-preview-carve-photos.png" 
           alt="Kitchen background"
           className="w-full h-full object-cover"
         />
