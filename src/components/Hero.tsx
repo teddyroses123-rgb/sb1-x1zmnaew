02d504d134664c6d3711c75a556eff6e112ce4c6
teddyroses123-rgb <teddyroses123@gmail.com>
2025-09-11 17:55:45 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
       {/* Background */}
       <div className="absolute inset-0">
         <img 
-          src="https://i.ibb.co/PvN7wXY7/image.png" 
+          src="https://i.ibb.co/V0gMw7fn/E6-DCA96-B-AF56-4-CA1-BAB1-3-C95196837-D0-no-bg-preview-carve-photos.png" 
           alt="Kitchen background"
           className="w-full h-full object-cover"
           loading="eager"
@@ -111,11 +111,7 @@ const Hero = () => {
             rel="noopener noreferrer"
             className="w-8 h-8 bg-black rounded flex items-center justify-center hover:bg-gray-800 transition-colors"
           >
-            <img 
-              src="https://i.ibb.co/jPTV0Pmk/free-icon-tiktok-3669950.png" 
-              alt="TikTok"
-              className="w-4 h-4"
-            />
+          </a>
           <a 
             href="https://www.instagram.com/artassya?igsh=MXM3NGNxMDFtd3Vt" 
             target="_blank" 
@@ -242,5 +238,3 @@ const Hero = () => {
 };
 
 export default Hero;
-  )
-}
