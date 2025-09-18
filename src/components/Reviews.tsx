61eaef33ba171ebb1fe3359567d7a415a0cea551
teddyroses123-rgb <teddyroses123@gmail.com>
2025-09-18 15:21:33 +0300

    Updated Reviews.tsx

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -42,7 +42,6 @@ const Reviews = () => {
           </h2>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
           {reviews.map((review, index) => (
             <div 
